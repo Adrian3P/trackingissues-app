51c1b32a93145c1820086c09ae999dd05305a6d1
Adrian Ionut Patrascu <ap@greenbyte.dk>
2024-01-15 04:12:30 +0100

    Adding dummy data for routing, navigation, data fetching, caching, rendering

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+'use client'
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button onClick={() => console.log("Click")}>Add to Cart</button>
+    </div>
+  );
+}
+
+export default AddToCart
---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AddToCart from './AddToCart'
+// 'use client' // -> including this file in a JS bundle which means all the components in that bundle will be treated as client components. Use client component when absolutely necessary
+
+const ProductCard = () => {
+  return (
+    <div>
+    <AddToCart />
+    </div>
+  )
+}
+
+export default ProductCard
